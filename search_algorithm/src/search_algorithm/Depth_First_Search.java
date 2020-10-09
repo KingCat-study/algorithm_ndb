1660ddd07307b6d142561e6984dc8c0fd47a8b1d
KingCat-study <jihunrock@gmail.com>
2020-10-09 14:29:11 +0900

    백준 문제(1206)를 기준으로 DFS 이해하기

---
+++ search_algorithm/src/search_algorithm/Depth_First_Search.java
@@ -0,0 +1,66 @@
+package search_algorithm;
+
+import java.util.Scanner;
+
+//백준 1260 문제 기반
+public class Depth_First_Search {
+
+	static int[][] map;
+	static boolean[] checked;
+	
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in);
+		
+		int n = kb.nextInt(); //정점의 개수
+		int m = kb.nextInt(); //간선의 개수
+		int v = kb.nextInt(); //시작할 정점
+		
+		map = new int[n+1][n+1];
+		checked = new boolean[n+1];
+		
+		for (int i = 0; i < m; i++) {
+			int a = kb.nextInt();
+			int b = kb.nextInt();
+			
+			map[a][b] = 1;
+			map[b][a] = 1;
+		}
+		
+		kb.close();
+		
+		dfs(v);
+//		checked = new boolean[n+1];
+//		System.out.println();
+//		bfs(v);
+	}
+	
+	public static void dfs(int v) {
+		checked[v] = true;
+		System.out.print(v + " ");
+		
+		for (int i = 0; i < map.length; i++) {
+			if(map[v][i] == 1 && !checked[i]) {
+				dfs(i);
+			}
+		}
+	}
+	
+	/*public static void bfs(int v) {
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(v);
+		checked[v] = true;
+		
+		while(!q.isEmpty()) {
+			int a = q.poll();
+			System.out.print(a + " ");
+			
+			for (int i = 0; i < map.length; i++) {
+				if(map[a][i] == 1 && !checked[i]) {
+					checked[i] = true;
+					q.add(i);
+				}
+			}
+		}
+	}*/
+}
