5a037bd6b11517d474ff05ccd0b3e8844728e985
KingCat-study <jihunrock@gmail.com>
2020-11-12 16:20:10 +0900

    백준 문제 풀이 11650
    Point 클래스 implements Comparable<T>
    Arrays.sort
    
    사용

---
+++ beakjoon_code/src/stepbystep/twelve/Main_11650.java
@@ -0,0 +1,58 @@
+package stepbystep.twelve;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_11650 {
+
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in);
+		
+		int n = kb.nextInt();
+		Point[] arr = new Point[n];
+		
+		for (int i = 0; i < n; i++) {
+			int x = kb.nextInt();
+			int y = kb.nextInt();
+			arr[i] = new Point(x,y);
+		}
+		
+		kb.close();
+		
+		Arrays.sort(arr);
+		
+		for (Point p : arr) {
+			System.out.println(p.x + " " + p.y);
+		}
+	}
+}
+
+class Point implements Comparable<Point> {
+	
+	int x;
+	int y;
+	
+	public Point(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public int compareTo(Point o) {
+		if(this.x == o.x) {
+			if(this.y < o.y) {
+				return -1;
+			} else if(this.y == o.y){
+				return 0;
+			} else {
+				return 1;
+			}
+		} else if(this.x < o.x) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+}
