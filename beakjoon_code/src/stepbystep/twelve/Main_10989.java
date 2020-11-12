06b5a371a321f9cf799d912206656daf67d0cd4f
KingCat-study <jihunrock@gmail.com>
2020-11-12 16:19:10 +0900

    백준 문제 풀이 10989
    Arrays.Sort 사용

---
+++ beakjoon_code/src/stepbystep/twelve/Main_10989.java
@@ -0,0 +1,32 @@
+package stepbystep.twelve;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_10989 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		int [] arr = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		br.close();
+		
+		Arrays.sort(arr);
+		
+		for (int i = 0; i < n; i++) {
+			sb.append(arr[i]).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
