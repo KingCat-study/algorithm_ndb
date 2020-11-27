47affa5a577b2bdaed2bc131486386e9354f7907
KingCat-study <jihunrock@gmail.com>
2020-11-27 22:01:56 +0900

    StringBuilder 클래스
    직접 구현 및 사용

---
+++ data_structure/src/data_structure/MyStringBuilder.java
@@ -0,0 +1,24 @@
+package data_structure;
+
+import java.util.ArrayList;
+
+public class MyStringBuilder {
+	
+	private ArrayList<String> words;
+
+	public MyStringBuilder() {
+		this.words = new ArrayList<>();
+	}
+	
+	public void append(String word) {
+		this.words.add(word);
+	}
+	
+	public String toString() {
+		String str = "";
+		for(String w : this.words) {
+			str = str + w;
+		}
+		return str;
+	}
+}
---
+++ data_structure/src/data_structure/MyStringBuilder2.java
@@ -0,0 +1,27 @@
+package data_structure;
+
+import java.util.Arrays;
+
+public class MyStringBuilder2 {
+	
+	private int count = 0;
+	private char[] value;
+	
+	public MyStringBuilder2() {
+		this.value = new char[16];
+		this.count = 0;
+	}
+	
+	public void append(String word) {
+		int oldCount = count;
+		count = oldCount + word.length();
+		if(count > this.value.length) {
+			value = Arrays.copyOf(value, value.length * 2);
+		}
+		word.getChars(0, word.length(), value, oldCount);
+	}
+	
+	public String toString() {
+		return new String(value,0,count);
+	}
+}
