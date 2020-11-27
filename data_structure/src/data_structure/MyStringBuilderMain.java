243f0de0635b338b1a2f57dc6c80effecf6f8b1e
KingCat-study <jihunrock@gmail.com>
2020-11-27 22:01:30 +0900

    자료 구조 노트 프로젝트

---
+++ data_structure/src/data_structure/MyStringBuilderMain.java
@@ -0,0 +1,23 @@
+package data_structure;
+
+public class MyStringBuilderMain {
+
+	public static void main(String[] args) {
+		MyStringBuilder msb = new MyStringBuilder();
+		
+		System.out.println(msb.toString());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		System.out.println(sb.toString());
+		
+		MyStringBuilder2 msb2 = new MyStringBuilder2();
+		
+		msb2.append("11");
+		msb2.append("22");
+		msb2.append("33");
+		msb2.append("44");
+		
+		System.out.println(msb2.toString());
+	}
+}
